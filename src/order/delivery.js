ac48c03e96d4cb6fedee992135b4e9835eff01eb
amor4330 <amor4330@gmail.com>
2023-01-27 20:08:42 +0900

    help....me

--- src/order/delivery.js
+++ src/order/delivery.js
@@ -3,10 +3,12 @@ import Delivery2 from './Delivery2';
 
 //CSS파일 경로 링크
 import delivery from './delivery.module.css';
+import PickUp from './PickUp';
 
 const Delivery = () => {
 
-   const [visible, setVisible] = useState(false);
+   const [del_visible, setDel_visible] = useState(true);
+   const [pu_visible, setPu_visible] = useState(false)
 
    return (
       <>
@@ -17,16 +19,21 @@ const Delivery = () => {
          </div>
          {/* 주문방법 선택 버튼 */}
          <div className={delivery.orderbtn}>
-            <button className = {delivery.orderbtn1} type = "button" onClick={() => {setVisible(!visible)}}> 
-            {visible ? <img src="https://imgcdn.pji.co.kr/pc/img/orderway_1_on.png" /> :
+            <button className = {delivery.orderbtn1} id = "del_btn" type = "button" onClick={() => {setDel_visible(true)
+            setPu_visible(false)}}> 
+            {del_visible ? <img src="https://imgcdn.pji.co.kr/pc/img/orderway_1_on.png" /> :
             <img src = "https://imgcdn.pji.co.kr/pc/img/orderway_1_off.png" /> }
             </button>
 
-            <button className = {delivery.orderbtn1} type = "button" onClick={() => {setVisible(visible)}}>
-            {visible ? <img src="https://imgcdn.pji.co.kr/pc/img/orderway_2_off.png" /> :
-            <img src = "https://imgcdn.pji.co.kr/pc/img/orderway_2_on.png" /> }
+            <button className = {delivery.orderbtn1} id = "pu_btn" type = "button" onClick={() => {setDel_visible(false)
+            setPu_visible(true)}}>
+            {pu_visible ? <img src="https://imgcdn.pji.co.kr/pc/img/orderway_2_on.png" /> :
+            <img src = "https://imgcdn.pji.co.kr/pc/img/orderway_2_off.png" /> }
             </button>
-            {visible ? <Delivery2 /> : null}
+
+            {del_visible ? <Delivery2 /> : <PickUp/>}
+
+            <button>진행시켜</button>
          </div>
       </>
    )
