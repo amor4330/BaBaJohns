c793c068c796380d9044ef1e8ada2132d9dfd6d3
minjeong <jennygh2@hanmail.net>
2023-01-28 17:08:58 +0900

    home 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './Home';
 import Delivery from './order/delivery';
 import PickUp from './order/PickUp';
 import Menu from './halfandhalf/menu';
 import {BabaProvider } from './context/UserContext';
+import Payment from './payment/Payment';
+import Home from './Home/Home';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/menu" element={<Menu/>}/>
           <Route path = "/delivery" element={<Delivery/>}/>
           <Route path = "/pickUp" element={<PickUp/>}/>
+          <Route path = "/payment" element={<Payment/>}/>
       </Routes>
     </BabaProvider>
 
